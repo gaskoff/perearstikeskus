59aba0f205f5509efa05bfef3c5968fabe28b1cd
jee7 <raimondtunnel@hotmail.com>
2021-10-21 23:14:55 +0300

    Refactoring. JS to a separate file. Image path fix ("img" folder is for system images, "image" folder is customer images).

---
+++ bot.js
@@ -0,0 +1,32 @@
+
+function toggleChat() {
+    let display = document.getElementById("chat-container").style.display;
+
+    document.getElementById("chat-container").style.display = display === "none" ? "block" : "none"
+}
+
+function openChat(){
+    document.getElementById("chat-container").style.display = "block";
+}
+
+function closeChat(){
+    document.getElementById("chat-container").style.display = "none";
+}
+
+const chatHistory = document.getElementById("chat-history-text");
+const userInput = document.getElementById("user-input");
+const attrUser = document.createAttribute("class");
+attrUser.value = "chat-user";
+
+userInput.addEventListener("keyup", function(e) {
+    if (e.which === 13) {
+        const p = document.createElement("p");
+        p.innerText = e.target.value;
+        const attrUser = document.createAttribute("class");
+        attrUser.value = "chat-user";
+        p.setAttributeNode(attrUser);
+        chatHistory.appendChild(p);
+        //userInput.
+
+    }
+});
